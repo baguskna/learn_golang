dcef3374c162de57682df88e22a1cea19c77853d
Bagus Kurnia <bagus.kurnia@sirclo.com>
2022-11-10 20:42:06 +0700

    feat: add input with biodata

--- biodata.go
+++ biodata.go
@@ -2,50 +2,80 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"html/template"
+	"net/http"
 	"strings"
 )
 
 type biodata struct {
 	ID      int
-	name    string
-	address string
-	title   string
-	reason  string
+	Name    string
+	Address string
+	Title   string
+	Reason  string
 }
 
 func main() {
-	names := []string{"bagus", "kurnia", "ahmad", "jokowi", "agus"}
-	biodatas := []biodata{
-		{ID: 1, name: "Bagus", address: "Bengkulu", title: "Farmer", reason: "live suck"},
-		{ID: 2, name: "Kurnia", address: "Tangerang", title: "Teacher", reason: "live suck"},
-		{ID: 3, name: "Ahmad", address: "Jakarta", title: "Student", reason: "live suck"},
-		{ID: 4, name: "Jokowi", address: "Solo", title: "President", reason: "----"},
-		{ID: 5, name: "Agus", address: "Bekasi", title: "CEO", reason: "live suck"},
-	}
+	http.HandleFunc("/", routeRootGet)
+	http.HandleFunc("/data", routeSubmitPost)
 
-	getArgsCli := os.Args
-	secondArgs := getArgsCli[1]
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
 
-	var whichIndex int = -1
+}
 
-	for i, name := range names {
-		if name == strings.ToLower(secondArgs) {
-			whichIndex = i
+func routeRootGet(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tmpl := template.Must(template.New("form").ParseFiles("view.html"))
+		err := tmpl.Execute(w, nil)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
+		return
 	}
 
-	for i, data := range biodatas {
-		if i == whichIndex {
-			fmt.Println("ID : ", data.ID)
-			fmt.Println("Name : ", data.name)
-			fmt.Println("Address : ", data.address)
-			fmt.Println("Title : ", data.title)
-			fmt.Println("Reason : ", data.reason)
+	http.Error(w, "", http.StatusBadRequest)
+}
+
+func routeSubmitPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		tmpl := template.Must(template.New("result").ParseFiles("view.html"))
+
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
-	}
 
-	if whichIndex == -1 {
-		fmt.Println("data not found")
+		nameForm := r.FormValue("name")
+
+		names := []string{"bagus", "kurnia", "ahmad", "jokowi", "agus"}
+		biodatas := []biodata{
+			{ID: 1, Name: "Bagus", Address: "Bengkulu", Title: "Farmer", Reason: "live suck"},
+			{ID: 2, Name: "Kurnia", Address: "Tangerang", Title: "Teacher", Reason: "live suck"},
+			{ID: 3, Name: "Ahmad", Address: "Jakarta", Title: "Student", Reason: "live suck"},
+			{ID: 4, Name: "Jokowi", Address: "Solo", Title: "President", Reason: "----"},
+			{ID: 5, Name: "Agus", Address: "Bekasi", Title: "CEO", Reason: "live suck"},
+		}
+
+		var whichIndex int = -1
+
+		for i, name := range names {
+			if name == strings.ToLower(nameForm) {
+				whichIndex = i
+			}
+		}
+
+		for i, data := range biodatas {
+			if i == whichIndex {
+				if err := tmpl.Execute(w, data); err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+			}
+		}
+
+		if whichIndex == -1 {
+			http.Error(w, "", http.StatusBadRequest)
+		}
 	}
 }
